8436b8658f7224c7114c37b2e16f58c114e96606
Onyi (Dre) Anozie <88848554+onyianozie@users.noreply.github.com>
2022-02-12 18:41:09 -0500

    testing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
