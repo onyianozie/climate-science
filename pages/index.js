50cab99d83f6b2099fd8ac8adfa9a32fb6e535c7
Onyi (Dre) Anozie <88848554+onyianozie@users.noreply.github.com>
2022-02-13 20:56:00 -0500

    made adjustments to css file

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,26 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.card}>
+        <p className={styles.description}>
+          Which of the below statements about electricity is not true?
+        </p>
+        <div className={styles.column}>
+          <label className={styles.label}>
+            <input type="radio" value="option1" checked={true} />
+            Electricity is measure in units 
+          </label>
+          <label className={styles.label}>
+            <input type="radio" className="radio" value="option1" checked={true} />
+            Electricity flows at the speed of light
+          </label>
+          <label className={styles.label}>
+            <input type="radio" value="option1" checked={true} />
+            Electricity is a primary energy source
+          </label>
+          
+        </div>
+      </div>
     </div>
   )
 } 
